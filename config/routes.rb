31905c83781ca2898e190f8704b0f97ddfb0594d
Cesar Cachay <cesar@able.co>
2019-09-06 17:10:45 -0500

    Render home component & Articles component

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/index'
   root to: 'pages#index'
-  
-    namespace :api do
-      resources :articles
-    end
+  namespace :api do
+    resources :articles
+  end
 end
